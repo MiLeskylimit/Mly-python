dbe83d7133acae7b015b475ae99e053c55601297
mile <510318967@qq.com>
2018-12-20 16:37:49 +0800

    213

---
+++ learn/pytho-jc.py
@@ -0,0 +1,20 @@
+name = "mile"
+print('Your name is {my_name}'.format(my_name=name))
+
+strdd = ' ab cd '
+strdd.strip()
+print(strdd.strip())
+
+float_nm = 0.1 + 0.2
+print(float_nm)
+
+age = 22
+message = "Happy " + str(age) + "rd birthday!"
+print(message)
+
+print(5 + 3)
+print(4 * 2)
+print(12 - 4)
+print(int(24 / 3))
+
+
